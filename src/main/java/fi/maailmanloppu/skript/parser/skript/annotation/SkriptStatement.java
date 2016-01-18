084781344f0c717645a5f5334c426d21a18b1370
bensku <bmi@mbnet.fi>
2016-01-18 21:25:17 +0200

    Simple pattern-based parser

---
+++ src/main/java/fi/maailmanloppu/skript/parser/skript/annotation/SkriptStatement.java
@@ -0,0 +1,21 @@
+package fi.maailmanloppu.skript.parser.skript.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.HashMap;
+import java.util.Map;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface SkriptStatement {
+    
+    /**
+     * Pattern, using Skript special pattern syntax.
+     */
+    String pattern();
+    
+    /**
+     * Variables in this array are automatically parsed and added as first locals.
+     * Given names will be used for function context variable names.
+     */
+    String[] variables();
+}
---
+++ src/main/java/fi/maailmanloppu/skript/parser/skript/effect/SimpleEffect.java
@@ -0,0 +1,38 @@
+package fi.maailmanloppu.skript.parser.skript.effect;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.spongepowered.api.util.Tuple;
+
+import fi.maailmanloppu.skript.Skript;
+import fi.maailmanloppu.skript.parser.CallTask;
+import fi.maailmanloppu.skript.parser.skript.EffectProcessor;
+import fi.maailmanloppu.skript.parser.skript.annotation.SkriptStatement;
+import fi.maailmanloppu.skript.util.SkriptPattern;
+
+public interface SimpleEffect extends EffectProcessor {
+    
+    static Tuple<Boolean, List<String>> matchPattern(String pattern, String code) {
+        SkriptPattern parser = new SkriptPattern(pattern, code);
+        Optional<List<String>> resultOptional = parser.parse();
+        if (resultOptional.isPresent()) {
+            List<String> result = resultOptional.get();
+            
+            return new Tuple<Boolean, List<String>>(true, result);
+        }
+        
+        return new Tuple<Boolean, List<String>>(false, null);
+    }
+    
+    @Override
+    default Optional<CallTask> check(String line) {
+        Class<? extends SimpleEffect> tc = this.getClass();
+        SkriptStatement annotation = tc.getAnnotation(SkriptStatement.class);
+        
+        Tuple<Boolean, List<String>> result = matchPattern(annotation.pattern(), line);
+        List<String> varsToParse = result.getSecond();
+        
+        return null;
+    }
+}
